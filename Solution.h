9955c352cc5471a1be7d9d694110df38fc308131
xujinming <xujinming@lixiang.com>
2021-11-04 19:29:42 +0800

    ddd

---
+++ Solution.h
@@ -0,0 +1,24 @@
+﻿#ifndef CSOLUTION_H
+#define CSOLUTION_H
+
+#include<iostream>
+#include<math.h>
+
+
+class Solution
+{
+public:
+    Solution();
+    virtual ~Solution();
+    void Function() {
+        std::cout<<"Hello world!"<<std::endl;
+    }
+
+protected:
+
+private:
+
+
+};
+
+#endif // CSOLUTION_H
